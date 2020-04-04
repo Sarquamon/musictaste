d4a8205d4ac1780de7f2ad70f80fc29261362a55
Salomón Piña <sarquamon@gmail.com>
2020-04-03 18:56:07 -0600

    Added logout btn and debugging logs, improved variable names for a better sessionStorage

--- src/Components/Tests/Tests.js
+++ src/Components/Tests/Tests.js
@@ -5,12 +5,14 @@ const musicTasteAPI = "http://localhost:9000";
 
 const Tests = () => {
   const [userLoggedIn, setUserLoggedIn] = useState(false);
-  const [userData, setUserData] = useState();
+  const [userData, setUserData] = useState({});
 
   const register = async e => {
+    console.log("register");
+
     e.preventDefault();
 
-    const userData = {
+    const registerUserData = {
       user__name: e.target.elements.userName.value,
       user__email: e.target.elements.userEmail.value,
       user__pwd: e.target.elements.registerUserPassword.value,
@@ -18,7 +20,7 @@ const Tests = () => {
       user__last_name: e.target.elements.userLastName.value
     };
 
-    Axios.post(`${musicTasteAPI}/user/register`, userData)
+    Axios.post(`${musicTasteAPI}/user/register`, registerUserData)
       .then(result => {
         console.log(`Result: ${result.data}`);
       })
@@ -28,23 +30,39 @@ const Tests = () => {
   };
 
   const login = async e => {
+    console.log("login");
+
     e.preventDefault();
-    const userData = {
+    const loginUserData = {
       user__pwd: e.target.elements.loginUserPassword.value,
       user__name: e.target.elements.userNameEmail.value,
       user__email: e.target.elements.userNameEmail.value
     };
-    Axios.post(`${musicTasteAPI}/user/login`, userData)
+    Axios.post(`${musicTasteAPI}/user/login`, loginUserData)
       .then(result => {
-        console.log(result.data);
-        setUserData(result.data);
+        const resultUserData = result.data;
+        console.log(resultUserData);
+        setUserData(resultUserData);
         setUserLoggedIn(true);
-        sessionStorage.setItem("userData", result.data);
+        sessionStorage.setItem("USER__NAME", resultUserData.Data.USER__NAME);
+        sessionStorage.setItem("USER__EMAIL", resultUserData.Data.USER__EMAIL);
+        sessionStorage.setItem("token", resultUserData.Data.token);
       })
       .catch(err => {
-        console.log(`Error! ${err}`);
+        // console.log(`Error! ${err}`);
         console.log(err.response);
       });
+  };
+
+  const logout = e => {
+    // console.log("logout");
+    e.preventDefault();
+
+    setUserLoggedIn(false);
+    setUserData({});
+    sessionStorage.removeItem("USER__NAME");
+    sessionStorage.removeItem("USER__EMAIL");
+    sessionStorage.removeItem("token");
   };
 
   return (
@@ -115,7 +133,15 @@ const Tests = () => {
           <button type="submit">Click Me!</button>
         </form>
       </div>
-      {userLoggedIn ? userData && <div>{userData.Data.USER__NAME}</div> : <></>}
+      {userLoggedIn ? (
+        <div>
+          {userData.Data.USER__NAME}
+
+          <button onClick={e => logout(e)}>Logout</button>
+        </div>
+      ) : (
+        <div>User not logged in</div>
+      )}
     </div>
   );
 };
