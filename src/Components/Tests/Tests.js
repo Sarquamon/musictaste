bcbf784487708ba46cec4a10cbcce54ac579fdf4
Salomón Piña <sarquamon@gmail.com>
2020-04-21 15:44:36 -0500

    Fixed typos on URL

--- src/Components/Tests/Tests.js
+++ src/Components/Tests/Tests.js
@@ -6,7 +6,7 @@ import Login from "./Login/Login";
 
 import "./Tests.css";
 
-const musicTasteAPI = "https://musictasteapi.azurewebsites.net/";
+const musicTasteAPI = "https://musictasteapi.azurewebsites.net";
 // const musicTasteAPI = "http://localhost:9000";
 
 const Tests = () => {
@@ -159,16 +159,16 @@ const Tests = () => {
 
     e.preventDefault();
 
-    Axios.get(`${musicTasteAPI}/spotify/random`);
+    // Axios.get(`${musicTasteAPI}/spotify/random`);
 
-    // Axios.get(`${musicTasteAPI}/spotify/getUserName`)
-    //   .then((result) => {
-    //     console.log("Success!\n", result.data);
-    //     setSpotifyUserData({ username: result.data.Details.display_name });
-    //   })
-    //   .catch((err) => {
-    //     console.log("Error!\n", err);
-    //   });
+    Axios.get(`${musicTasteAPI}/spotify/getUserName`)
+      .then((result) => {
+        console.log("Success!\n", result.data);
+        setSpotifyUserData({ username: result.data.Details.display_name });
+      })
+      .catch((err) => {
+        console.log("Error!\n", err);
+      });
   };
 
   const getRecommendedGenres = (e) => {
