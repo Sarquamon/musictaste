cf8ab5fc1f2347782a9cec1b90a5d0873e0b10af
Salomón Piña <sarquamon@gmail.com>
2020-04-20 21:08:09 -0500

    Fixed bugs

--- src/Routes/Router.js
+++ src/Routes/Router.js
@@ -2,13 +2,11 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "../Components/Home/Home";
 import Tests from "../Components/Tests/Tests";
-import Pruebas from "../Components/Pruebas/Pruebas";
 
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={Home} />
     <Route exact path="/tests" component={Tests} />
-    <Route exact path="/pruebas" component={Pruebas} />
   </Switch>
 );
 
